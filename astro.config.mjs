7748a3be77256fac920a793d8c7be5ef3cb71cf8
Bobby Muljono [SP4756] <bobby.muljono@shopee.com>
2026-07-09 17:46:57 +0800

    can inspect element from localhost now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import { codeInspectorPlugin } from 'code-inspector-plugin';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bobbymuljono.github.io',
   base: '/',
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+  vite: {
+    plugins: [codeInspectorPlugin({ bundler: 'vite' })],
+  },
 });
